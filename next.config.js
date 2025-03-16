e0b216d8562b1f8a1acb578235402ddf558107fc
César Couto <cesar@cesarcouto.com>
2025-03-15 23:54:01 -0100

    Update Next.js configuration to disable dev indicators and enable turbopack in development script

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     // This is the updated configuration
     serverExternalPackages: ['better-sqlite3'],
+    devIndicators: false
 
     // Keep any other configuration options you might have
 }
